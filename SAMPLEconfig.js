a93541f4cfdc980d021841b7a019981f65f60f03
Matt Thompson <matt@thompsn.com>
2017-01-15 15:44:13 -0600

    init

---
+++ SAMPLEconfig.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.firetable = {};
+config.youtube = {};
+config.firebase = {};
+
+config.firetable.username = "xxxxx";
+config.firetable.password = "xxxxx";
+
+config.youtube.key = "xxxxx";
+
+config.firebase.key = "xxxxx";
+config.firebase.auth = "xxxx";
+config.firebase.db = "xxxx";
+
+module.exports = config;
